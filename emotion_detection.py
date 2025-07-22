ddb3d9c831005d604269a3eb879ef9397b2929d7
AriadnaJV <ariadnavianna@gmail.com>
2025-07-22 18:03:48 -0400

    Final commit

--- emotion_detection.py
+++ emotion_detection.py
@@ -8,21 +8,14 @@ def emotion_detector(text_to_analyse):
     response = requests.post(url, json = myobj, headers = header)
     formatted_response = json.loads(response.text)
 
-    if response.status_code == 400:
-        anger_score = None
-        disgust_score = None
-        fear_score = None
-        joy_score = None
-        sadness_score = None
-        dominant_emotion = None
-    else:
-        emotions = formatted_response['emotionPredictions'][0]['emotion']
-        anger_score = emotions['anger']
-        disgust_score = emotions['disgust']
-        fear_score = emotions['fear']
-        joy_score = emotions['joy']
-        sadness_score = emotions['sadness']
-        dominant_emotion = max(emotions, key = emotions.get)
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+
+    anger_score = emotions['anger']
+    disgust_score = emotions['disgust']
+    fear_score = emotions['fear']
+    joy_score = emotions['joy']
+    sadness_score = emotions['sadness']
+    dominant_emotion = max(emotions, key = emotions.get)
 
     return {
         "anger": anger_score,
